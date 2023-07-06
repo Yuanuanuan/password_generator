beb6ac2c2a1c6cee780da51ae035ee1b68cb6ad4
Yuanuanuan <asdll6618@gmail.com>
2023-07-06 15:11:25 +0800

    build: main.js add: app.js & styles.css & index.hbs

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const { engine } = require('express-handlebars')
 const app = express();
 const port = 3000;
+const generatePassword = require('./main')
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs');
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const option = req.body;
+  const password = generatePassword(option);
+  res.render('index', {password, option})
 })
 
 app.listen(port, () => {
